a6ea1743e3d4409503162cf3f0d3aed5c97032eb
mshapir <mshapir95@gmail.com>
2019-05-01 15:10:49 -0400

    navbar simple styling

--- personalwebsite/src/MenuBar.js
+++ personalwebsite/src/MenuBar.js
@@ -4,8 +4,14 @@ class MenuBar extends Component {
 
   render() {
     return (
-      <div>
-      Menu Bar here 
+      <div className="menubar">
+
+        <a href='#' className="link">About</a>
+        <a href='#' className="link">Projects</a>
+        <h4 className="manny">Manny Shapir</h4>
+        <a href='#' className="link">Resume</a>
+        <a href='#' className="link">Social Media</a>
+
       </div>
     );
   }
