45b2afacd8bff7c4c36869a5c42fdfaf16b821fa
mshapir <mshapir95@gmail.com>
2019-05-03 15:09:08 -0400

    changed jumbotron pics

--- personalwebsite/src/Jumbotron.js
+++ personalwebsite/src/Jumbotron.js
@@ -7,7 +7,7 @@ class Jumbotron extends Component {
       <section id="colorlib-hero" class="js-fullheight" data-section="home">
         <div class="flexslider js-fullheight">
           <ul class="slides">
-            <li style={{backgroundImage: 'url(images/img_bg_1.jpg)'}}>
+            <li style={{backgroundImage: 'url(images/img_bg_4.jpeg)'}}>
               <div class="overlay"></div>
               <div class="container-fluid">
                 <div class="row">
@@ -23,13 +23,13 @@ class Jumbotron extends Component {
                 </div>
               </div>
             </li>
-            <li style={{backgroundImage: 'url(images/img_bg_2.jpg)'}}>
+            <li style={{backgroundImage: 'url(images/img_bg_3.jpg)'}}>
               <div class="overlay"></div>
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                  <div class="col-md-6 col-md-offset-9 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                     <div class="slider-text-inner">
-                      <div class="desc">
+                      <div class="desc" style={{textAlign: 'right'}}>
                         <h1>I am an <br/>engineer,</h1>
                         <h2>and I love to build things.</h2>
                         {/*<p><a class="btn btn-primary btn-learn">View Portfolio <i class="icon-briefcase3"></i></a></p>*/}
